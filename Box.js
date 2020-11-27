e82ff8094e7de3afd53bf535d68adff8805ad9ab
yogitaGarg12345 <75129225+yogitaGarg12345@users.noreply.github.com>
2020-11-27 06:19:28 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box{
+    constructor(x,y,width,height){
+        var options ={
+            'restitution':0.8
+
+        }
+this.body = Bodies.rectangle(x,y,width,height,options)
+this.width = width
+this.height = height
+
+World.add(world,this.body)
+
+
+    }
+    display(){
+    var pos = this.body.position
+    rectMode(CENTER)
+    Fill("red")
+    rect(pos.x,pos.y,this.width,this.height)
+
+    }
+
+
+
+
+
+
+}
